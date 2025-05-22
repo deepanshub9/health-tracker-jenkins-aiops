b5cdb809cef7c7a775541f571b602d553bd7afbe
Deepanshu <kukukumar1996@gmail.com>
2025-05-22 09:30:42 +0100

    Dbconfig file H2 memory Update

--- src/main/kotlin/ie/setu/config/Dbconfig.kt
+++ src/main/kotlin/ie/setu/config/Dbconfig.kt
@@ -3,8 +3,6 @@ package ie.setu.config
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.jetbrains.exposed.sql.Database
 import org.postgresql.util.PSQLException
-import org.jetbrains.exposed.sql.name
-
 
 class DbConfig {
 
@@ -12,28 +10,39 @@ class DbConfig {
     private lateinit var dbConfig: Database
 
     fun getDbConnection(): Database {
+        val dbType = System.getenv("DB_TYPE") ?: "postgres"  // default to postgres
 
-        val PGHOST = "dpg-cvc253jtq21c73e5dd50-a.frankfurt-postgres.render.com"
-        val PGPORT = "5432"
-        val PGUSER = "health_tracker_gkwy_user"
-        val PGPASSWORD = "xxPJzKaqaAkB191XVZGghL7oytS5Q0Jh"
-        val PGDATABASE = "health_tracker_gkwy"
+        return try {
+            if (dbType == "h2") {
+                logger.info { "Connecting to H2 in-memory database for tests..." }
+                dbConfig = Database.connect(
+                    url = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;",
+                    driver = "org.h2.Driver",
+                    user = "root",
+                    password = ""
+                )
+            } else {
+                val PGHOST = System.getenv("PGHOST") ?: "dpg-cvc253jtq21c73e5dd50-a.frankfurt-postgres.render.com"
+                val PGPORT = System.getenv("PGPORT") ?: "5432"
+                val PGUSER = System.getenv("PGUSER") ?: "health_tracker_gkwy_user"
+                val PGPASSWORD = System.getenv("PGPASSWORD") ?: "xxPJzKaqaAkB191XVZGghL7oytS5Q0Jh"
+                val PGDATABASE = System.getenv("PGDATABASE") ?: "health_tracker_gkwy"
 
-        //url format should be jdbc:postgresql://host:port/database
-        val dbUrl = "jdbc:postgresql://$PGHOST:$PGPORT/$PGDATABASE"
+                val dbUrl = "jdbc:postgresql://$PGHOST:$PGPORT/$PGDATABASE"
+                logger.info { "Connecting to PostgreSQL... $dbUrl" }
 
-        try {
-            logger.info { "Starting DB Connection...$dbUrl" }
-            dbConfig = Database.connect(
-                url = dbUrl, driver = "org.postgresql.Driver",
-                user = PGUSER, password = PGPASSWORD
-            )
-            logger.info { "DB Connected Successfully..." + dbConfig.url }
+                dbConfig = Database.connect(
+                    url = dbUrl,
+                    driver = "org.postgresql.Driver",
+                    user = PGUSER,
+                    password = PGPASSWORD
+                )
+            }
+            logger.info { "Database connected successfully." }
+            dbConfig
         } catch (e: PSQLException) {
-            logger.info { "Error in DB Connection...${e.printStackTrace()}" }
+            logger.error(e) { "Error during DB connection!" }
+            throw e
         }
-
-        return dbConfig
-
     }
 }
